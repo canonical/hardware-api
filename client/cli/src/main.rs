9d1f7e0f242bac7471b01ceeac6650c22988b13d
Matias Piipari <matias.piipari@gmail.com>
2024-01-18 10:38:43 +0200

    Some initial dependencies

--- client/cli/src/main.rs
+++ client/cli/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use hilib::get_motherboard_info_and_send;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = get_motherboard_info_and_send().await {
+        eprintln!("Failed to process motherboard info: {}", e);
+    }
 }
--- client/lib/src/lib.rs
+++ client/lib/src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Serialize, Deserialize};
+use dmidecode_rs::DmiDecoder;
+use reqwest;
+
+#[derive(Serialize, Deserialize)]
+pub struct MotherboardInfo {
+    pub manufacturer: String,
+    pub product_name: String,
+    // ... other fields ...
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn get_motherboard_info_and_send() -> Result<(), Box<dyn std::error::Error>> {
+    let info = fetch_motherboard_info()?;
+    send_to_web_service(&info).await?;
+    Ok(())
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn fetch_motherboard_info() -> Result<MotherboardInfo, Box<dyn std::error::Error>> {
+    let decoder = DmiDecoder::new()?;
+    let mut manufacturer = String::new();
+    let mut product_name = String::new();
+
+    for structure in decoder.decode()? {
+        if let Some(baseboard) = structure.baseboard() {
+            manufacturer = baseboard.manufacturer().unwrap_or_default().to_owned();
+            product_name = baseboard.product().unwrap_or_default().to_owned();
+        }
+    }
+
+    Ok(MotherboardInfo {
+        manufacturer,
+        product_name,
+        // ... other fields ...
+    })
+}
+
+async fn send_to_web_service(info: &MotherboardInfo) -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = client.post("http://your-web-service-url.com/api")
+        .json(&info)
+        .send()
+        .await?;
+    
+    if res.status().is_success() {
+        Ok(())
+    } else {
+        Err(res.error_for_status().unwrap_err())
     }
 }
+
