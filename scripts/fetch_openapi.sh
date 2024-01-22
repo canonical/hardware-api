58e0da4a512cfe339926abb45830b3c607b505e9
nadzyah <nadzeya.hutsko@canonical.com>
2024-01-22 14:34:21 +0200

    Add pre-commit hook for OpenAPI schema fetching

---
+++ scripts/fetch_openapi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd server
+
+poetry install
+
+# Start FastAPI app in the background
+poetry run uvicorn hwapi.main:app --port 8002 &
+
+# Wait for the server to start
+sleep 5
+
+# Fetch OpenAPI YAML
+curl http://localhost:8002/openapi.yaml -o openapi.yaml
+
+# Kill the FastAPI server
+kill $!
