261c6804bbe540add87ab41fce60f9e3e421c7c1
Paul Larson <paul.larson@canonical.com>
2024-05-02 13:21:35 -0500

    Terraform fixes to use nginx ingress

--- server/terraform/hardware-api.tf
+++ server/terraform/hardware-api.tf
@@ -13,10 +13,16 @@ variable "environment" {
   description = "The environment to deploy to (development, staging, production)"
 }
 
+variable "tls_secret_name" {
+  description = "Secret where the TLS certificate for ingress is stored"
+  type        = string
+  default     = ""
+}
+
 variable "external_ingress_hostname" {
   description = "External hostname for the ingress"
   type        = string
-  default     = "hardware-api.ubuntu.com"
+  default     = "hw.ubuntu.com"
 }
 
 resource "juju_model" "hardware-api" {
@@ -33,21 +39,37 @@ resource "juju_application" "hardware-api" {
   }
 
   config = {
-    hostname = var.environment == "staging" ? "hardware-api-staging.${var.external_ingress_hostname}" : "hardware-api.${var.external_ingress_hostname}"
+    hostname = var.external_ingress_hostname
     port     = var.environment == "development" ? 30000 : 443
   }
 
   units = 1
 }
 
-# resource "juju_integration" "hardware-api-ingress" {
-#   model = juju_model.hardware-api.name
-#
-#   application {
-#     name = juju_application.hardware-api.name
-#   }
-#
-#   application {
-#     name = juju_application.ingress.name
-#   }
-# }
+resource "juju_application" "ingress" {
+  name  = "ingress"
+  model = juju_model.hardware-api.name
+  trust = true
+
+  charm {
+    name    = "nginx-ingress-integrator"
+    channel = "latest/stable"
+  }
+
+  config = {
+    tls-secret-name = var.tls_secret_name
+  }
+}
+
+
+resource "juju_integration" "hardware-api-ingress" {
+  model = juju_model.hardware-api.name
+
+  application {
+    name = juju_application.hardware-api.name
+  }
+
+  application {
+    name = juju_application.ingress.name
+  }
+}
