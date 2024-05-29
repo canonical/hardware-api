c4bfd7a6f5268c320b25b77b4355e24009bd7ee0
Nadzeya H <84857215+nadzyah@users.noreply.github.com>
2024-05-29 16:03:48 +0200

    Add coverage command and PR annotation (#139)
    * Add coverage command and PR annotation
    
    * Use v4 artefact action

--- scripts/generate_openapi_html.sh
+++ scripts/generate_openapi_html.sh
@@ -5,6 +5,11 @@ if git diff --cached --name-only | grep --quiet "server"
 then
     cd server/schemas/
 
+    if ! command -v npx
+    then
+        echo "Install nodejs according to the instructions in server/README.md"
+        exit 1
+    fi
     # Generate HTML schema
     npx @redocly/cli build-docs openapi.yaml -o openapi.html
 
