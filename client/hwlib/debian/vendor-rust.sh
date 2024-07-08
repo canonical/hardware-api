4375eac4a21e396068bac74a26217427816cf07b
Nadzeya Hutsko-Hryshalevich <84857215+nadzyah@users.noreply.github.com>
2024-07-08 13:14:46 +0300

    Update debian directory for hwlib (#169)
    * Update debian/* files
    
    * Normalise Cargo.toml
    
    * Remove checksum for .a files
    
    * Add GH CI for testing the hwlib debian build
    
    * Address the comments
    
    * Fix typo in CI
    
    * Exclude only top debian/ and vendor/ dirs from cargo-checksum.json

--- client/hwlib/debian/vendor-rust.sh
+++ client/hwlib/debian/vendor-rust.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -eu
+
+CARGO_VENDOR_DIR="${CARGO_VENDOR_DIR:-vendor}"
 
 # We need a filtered vendored directory
 if [ ! $(which cargo-vendor-filterer) ]; then
@@ -7,7 +10,15 @@ if [ ! $(which cargo-vendor-filterer) ]; then
     exit 3
 fi
 
+cargo vendor-filterer "$CARGO_VENDOR_DIR"
+
 # Some crates are shipped with .a files, which get removed by the helpers during the package build as a safety measure.
 # This results in cargo failing to compile, since the files (which are listed in the checksums) are not there anymore.
-# For those crates, we need to replace their checksum with a more general one that only lists the crate checksum, instead of each file.
-cargo vendor-filterer "${CARGO_VENDOR_DIR:-vendor}"
+# For those crates, we need to remove their checksums
+for json_file in $(find "$CARGO_VENDOR_DIR" -name ".cargo-checksum.json"); do
+    tmp_file=$(mktemp)
+    jq '.files |= with_entries(select(.key | endswith(".a") | not))' "$json_file" > "$tmp_file"
+    mv "$tmp_file" "$json_file"
+done
+
+echo "Filtered vendored dependencies and updated checksums."
