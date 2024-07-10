4fd0ec55d7e439cd6574f061db60186538be15a4
nadzyah <nadzeya.hutsko@canonical.com>
2024-07-10 14:36:30 +0300

    Remove checksum for vcpkg/*.sh files that are excluded from archive

--- client/hwlib/debian/vendor-rust.sh
+++ client/hwlib/debian/vendor-rust.sh
@@ -15,14 +15,20 @@ cargo vendor-filterer "$CARGO_VENDOR_DIR"
 # Delete .sh files in the vendor/vcpkg directory if it exists
 if [ -d "$CARGO_VENDOR_DIR/vcpkg" ]; then
     find "$CARGO_VENDOR_DIR/vcpkg" -name "*.sh" -exec rm -f {} +
+    # Remove checksums for the deleted *.sh files that are not used by the code but generate lintian errors
+    for json_file in $(find "${CARGO_VENDOR_DIR}/vcpkg" -name ".cargo-checksum.json"); do
+        tmp_file=$(mktemp)
+        jq '.files |= with_entries(select(.key | endswith(".sh") | not))' "$json_file" > "$tmp_file"
+        mv "$tmp_file" "$json_file"
+    done
 fi
 
 # Some crates are shipped with .a files, which get removed by the helpers during the package build as a safety measure.
-# This results in cargo failing to compile, since the files (which are listed in the checksums) are not there anymore. For those crates, we need to remove their checksums.
-# Also, here we remove checksums for the deleted *.sh files that are not used by the code but generate lintian errors
+# This results in cargo failing to compile, since the files (which are listed in the checksums) are not there anymore.
+# For those crates, we need to remove their checksums
 for json_file in $(find "$CARGO_VENDOR_DIR" -name ".cargo-checksum.json"); do
     tmp_file=$(mktemp)
-    jq '.files |= with_entries(select(.key | endswith(".a") | not) | select(.key | endswith(".sh") | not))' "$json_file" > "$tmp_file"
+    jq '.files |= with_entries(select(.key | endswith(".a") | not))' "$json_file" > "$tmp_file"
     mv "$tmp_file" "$json_file"
 done
 
