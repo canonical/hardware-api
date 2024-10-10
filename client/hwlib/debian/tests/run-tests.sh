e248805fc2504227ac5e0e634a2e68311381053b
Nadzeya H <nadzeya.hutsko@canonical.com>
2024-10-10 17:02:50 +0300

    Update .cargo dir name in run-tests.sh

--- client/hwlib/debian/tests/run-tests.sh
+++ client/hwlib/debian/tests/run-tests.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+set -xeu
 
 # Create cargo config directory
-mkdir -p /tmp/.cargo
+mkdir -p /tmp/.cargo-autopkgtest
 
 # Create config.toml
-cat > /tmp/.cargo/config.toml << EOL
+cat > /tmp/.cargo-autopkgtest/config.toml << EOL
 [source.crates-io]
 replace-with = "vendored-sources"
 
@@ -13,4 +14,4 @@ directory = "$(pwd)/rust-vendor"
 EOL
 
 # Run the actual test
-CARGO_HOME=/tmp/.cargo/ cargo test --offline --manifest-path Cargo.toml --all-targets --all-features
+CARGO_HOME=/tmp/.cargo-autopkgtest/ cargo test --offline --manifest-path Cargo.toml --all-targets --all-features
