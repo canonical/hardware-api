84d40293a81e1b2415304e3151379778133fbb1c
Jonathan Cave <jonathan.cave@canonical.com>
2024-11-26 13:28:53 +0000

    License additions (#245)
    * The hardware-api server should be AGPLv3 licensed
    
    This project maintains a monorepo with components distributed
    in separate packages. The components should use the appropriate
    open source licenses.
    
    Note that the charm for the server is Apache Licensed and a
    license file already exists in that directory.
    
    * The hardware-api hwlib should be LGPLv3 licensed
    
    This project maintains a monorepo with components distributed
    in separate packages. The components should use the appropriate
    open source licenses.
    
    * The hardware-api hwctl should be GPLv3 licensed
    
    This project maintains a monorepo with components distributed
    in separate packages. The components should use the appropriate
    open source licenses.
    
    * Update the files headers in hwapi server side to use GNU AGPLv3
    
    * Update the files headers in hwlib/src/ and debian/ to use GNU LGPLv3
    
    * Fix file headers to use only *GPLv3 license and remove Copyright notice.
    
    ---------
    
    Co-authored-by: Nadzeya H <nadzeya.hutsko@canonical.com>

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,10 +1,8 @@
 # Copyright 2024 Canonical Ltd.
-# All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
