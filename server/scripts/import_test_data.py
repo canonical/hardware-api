b4cce3ab964e14156c1419041fa53f51d8e5b4f4
Nadzeya H <nadzeya.hutsko@canonical.com>
2024-11-26 16:11:32 +0200

    Update integration tests README.md with tests description

--- server/scripts/import_test_data.py
+++ server/scripts/import_test_data.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
-"""Script for importing test machine data from C3 with mocked responses"""
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Written by:
+#        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
+"""Script for importing test machine data using mocked C3 responses"""
 
 import os
 import json
