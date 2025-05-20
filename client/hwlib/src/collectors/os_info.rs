48af2c39b60b0c5645144c1380683729bd5b41cb
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-05-20 11:17:06 -0400

    Add missing Context use

--- client/hwlib/src/collectors/os_info.rs
+++ client/hwlib/src/collectors/os_info.rs
@@ -16,7 +16,7 @@
  *        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
  */
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{fs::read_to_string, path::Path, process::Command};
