eb1e1cc6f18060c3b57bf91f72ca6e745eb42199
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-05-21 09:49:35 -0400

    Simplify build.rs

--- client/hwlib/build.rs
+++ client/hwlib/build.rs
@@ -1,8 +1,5 @@
 fn main() {
     let os_release_file_path =
-        std::env::var("OS_RELEASE_FILE_PATH").unwrap_or("/etc/os-release".to_string());
-    println!(
-        "cargo:rustc-env=OS_RELEASE_FILE_PATH={}",
-        os_release_file_path
-    );
+        std::env::var("OS_RELEASE_FILE_PATH").unwrap_or("/etc/os-release".to_owned());
+    println!("cargo:rustc-env=OS_RELEASE_FILE_PATH={os_release_file_path}");
 }
