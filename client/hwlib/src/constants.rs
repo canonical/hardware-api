5f59ed47c0f33fb0be3ec4bed74f18526826387e
Pedro Avalos <pedro.avalosjimenez@canonical.com>
2025-05-21 08:56:03 -0400

    Update client/hwlib/src/constants.rs
    Co-authored-by: Nadzeya H <84857215+nadzyah@users.noreply.github.com>

--- client/hwlib/src/constants.rs
+++ client/hwlib/src/constants.rs
@@ -23,7 +23,7 @@ pub const PROC_CPUINFO_FILE_PATH: &str = "/proc/cpuinfo";
 pub const PROC_DEVICE_TREE_DIR_PATH: &str = "/proc/device-tree/";
 pub const PROC_VERSION_FILE_PATH: &str = "/proc/version";
 
-// The os-release file path is set by a build-time environment variable
+// The os-release file path is set during the build time
 // to allow for snap packaging, which exposes the file in a different location.
 pub const OS_RELEASE_FILE_PATH: &str = env!("OS_RELEASE_FILE_PATH", "/etc/os-release");
 
