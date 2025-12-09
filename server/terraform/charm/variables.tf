12e491991d6f326b84406e18b8885ad9fb9a72a2
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-12-09 17:20:16 -0500

    feat: add charm terraform files

---
+++ server/terraform/charm/variables.tf
@@ -0,0 +1,50 @@
+variable "app_name" {
+  type        = string
+  default     = "api"
+  description = "Name to give the deployed application"
+}
+
+variable "base" {
+  type        = string
+  nullable    = true
+  default     = null
+  description = "The operating system on which to deploy"
+}
+
+variable "channel" {
+  type        = string
+  default     = "latest/edge"
+  description = "Channel of the charm"
+}
+
+variable "config" {
+  type        = map(string)
+  default     = {}
+  description = "Map for config options"
+}
+
+variable "constraints" {
+  type        = string
+  default     = "arch=amd64"
+  description = "String listing constraints for this application"
+}
+
+variable "model" {
+  type        = string
+  nullable    = true
+  default     = null
+  description = "Reference to an existing model resource or data source for the model to deploy to"
+}
+
+variable "revision" {
+  type        = number
+  nullable    = true
+  default     = null
+  description = "Revision number of the charm"
+}
+
+variable "units" {
+  type        = number
+  default     = 1
+  description = "Unit count/scale"
+}
