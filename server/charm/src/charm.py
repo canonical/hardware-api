5d4d71bd3bf3746e8e02daea3e09a4e9f207e247
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-12-11 15:48:46 -0500

    feat(charm): use pydantic to validate configuration

--- server/charm/src/charm.py
+++ server/charm/src/charm.py
@@ -7,14 +7,22 @@
 
 import logging
 import shlex
+from typing import Literal
 
 import ops
+import pydantic
 from charms.nginx_ingress_integrator.v0.nginx_route import require_nginx_route
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
-VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
+
+class HardwareApiConfig(pydantic.BaseModel):
+    """Hardware API Charm configuration."""
+
+    log_level: Literal["info", "debug", "warning", "error", "critical"] = "info"
+    port: int = 30000
+    hostname: str = "str"
 
 
 class HardwareApiCharm(ops.CharmBase):
@@ -22,6 +30,7 @@ class HardwareApiCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.typed_config = self.load_config(HardwareApiConfig, errors="blocked")
         self._setup_nginx()
         self.framework.observe(
             self.on["hardware-api"].pebble_ready,
@@ -32,9 +41,9 @@ class HardwareApiCharm(ops.CharmBase):
     def _setup_nginx(self):
         require_nginx_route(
             charm=self,
-            service_hostname=str(self.config["hostname"]),
+            service_hostname=self.typed_config.hostname,
             service_name=self.app.name,
-            service_port=int(self.config["port"]),
+            service_port=self.typed_config.port,
         )
 
     def _on_hardware_api_pebble_ready(self, event: ops.PebbleReadyEvent):
@@ -44,11 +53,6 @@ class HardwareApiCharm(ops.CharmBase):
         self.unit.status = ops.ActiveStatus()
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        log_level = str(self.model.config["log-level"]).lower()
-        if log_level not in VALID_LOG_LEVELS:
-            self.unit.status = ops.BlockedStatus(f"invalid log level: '{log_level}'")
-            return
-
         container = self.unit.get_container("hardware-api")
         if not container.can_connect():
             self.unit.status = ops.WaitingStatus("waiting for Pebble API")
@@ -57,7 +61,7 @@ class HardwareApiCharm(ops.CharmBase):
 
         container.add_layer("hardware-api", self._pebble_layer, combine=True)
         container.replan()
-        logger.debug("Log level changed to '%s'", log_level)
+        logger.debug("Log level changed to '%s'", self.typed_config.log_level)
         self.unit.status = ops.ActiveStatus()
 
     @property
@@ -82,9 +86,9 @@ class HardwareApiCharm(ops.CharmBase):
                             "--host",
                             "0.0.0.0",
                             "--port",
-                            f"{self.config['port']}",
+                            f"{self.typed_config.port}",
                             "--log-level",
-                            f"{self.model.config['log-level']}",
+                            f"{self.typed_config.log_level}",
                         ]
                     ),
                     "startup": "enabled",
