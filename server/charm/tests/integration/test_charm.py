584e0a4542d4d301b1eb85cc7ceea84f341a7baa
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-01-16 16:43:47 -0500

    chore(charm): add app is up integration test

--- server/charm/tests/integration/test_charm.py
+++ server/charm/tests/integration/test_charm.py
@@ -9,6 +9,8 @@ from pathlib import Path
 import jubilant
 import yaml
 
+from .helpers import app_is_up, retry
+
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("charmcraft.yaml").read_text(encoding="utf-8"))
@@ -23,6 +25,14 @@ def test_deploy(charm: Path, juju: jubilant.Juju):
     config = {"port": PORT}
     juju.deploy(charm.resolve(), app=APP_NAME, resources=resources, config=config)
     juju.wait(jubilant.all_active)
+
+
+@retry(retry_num=5, retry_sleep_sec=5)
+def test_application_is_up(juju: jubilant.Juju):
+    """Test that the application is up and running."""
+    ip = juju.status().apps[APP_NAME].units[f"{APP_NAME}/0"].address
+    base_url = f"http://{ip}:{PORT}"
+    assert app_is_up(base_url)
 
 
 def test_relate_ingress(juju: jubilant.Juju):
