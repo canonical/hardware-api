6c4d39acf3c8491ef9e3fa2551e4e7df61436fce
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-01-26 14:31:40 -0500

    chore(charm): add integration test that app is up

--- server/charm/tests/integration/test_charm.py
+++ server/charm/tests/integration/test_charm.py
@@ -9,19 +9,30 @@ from pathlib import Path
 import jubilant
 import yaml
 
+from .helpers import app_is_up, retry
+
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("charmcraft.yaml").read_text(encoding="utf-8"))
 APP_NAME = METADATA["name"]
+PORT = 80
 
 
 def test_deploy(charm: Path, juju: jubilant.Juju):
     """Deploy the charm under test."""
-    resources = {
-        "hardware-api-image": METADATA["resources"]["hardware-api-image"]["upstream-source"],
-    }
-    juju.deploy(charm.resolve(), app=APP_NAME, resources=resources)
+    upstream_source = METADATA["resources"]["hardware-api-image"]["upstream-source"]
+    resources = {"hardware-api-image": upstream_source}
+    config = {"port": PORT}
+    juju.deploy(charm.resolve(), app=APP_NAME, resources=resources, config=config)
     juju.wait(jubilant.all_active)
+
+
+@retry(retry_num=5, retry_sleep_sec=5)
+def test_application_is_up(juju: jubilant.Juju):
+    """Test that the application is up and running."""
+    ip = juju.status().apps[APP_NAME].units[f"{APP_NAME}/0"].address
+    base_url = f"http://{ip}:{PORT}"
+    assert app_is_up(base_url)
 
 
 def test_relate_ingress(juju: jubilant.Juju):
