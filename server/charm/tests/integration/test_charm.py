d65fd8057e40190bbe4bb1c8ef2d7be25a3df3e2
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-01-26 15:01:18 -0500

    chore(charm): expand ingress integration test

--- server/charm/tests/integration/test_charm.py
+++ server/charm/tests/integration/test_charm.py
@@ -4,25 +4,29 @@
 """Integration tests for the Hardware API Charm."""
 
 import logging
+import re
 from pathlib import Path
 
 import jubilant
+import requests
 import yaml
 
-from .helpers import app_is_up, retry
+from .helpers import DNSResolverHTTPAdapter, app_is_up, retry
 
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("charmcraft.yaml").read_text(encoding="utf-8"))
 APP_NAME = METADATA["name"]
 PORT = 80
+INGRESS_NAME = "ingress"
+EXTERNAL_HOSTNAME = "hw.internal"
 
 
 def test_deploy(charm: Path, juju: jubilant.Juju):
     """Deploy the charm under test."""
     upstream_source = METADATA["resources"]["hardware-api-image"]["upstream-source"]
     resources = {"hardware-api-image": upstream_source}
-    config = {"port": PORT}
+    config = {"port": PORT, "hostname": EXTERNAL_HOSTNAME}
     juju.deploy(charm.resolve(), app=APP_NAME, resources=resources, config=config)
     juju.wait(jubilant.all_active)
 
@@ -35,8 +39,23 @@ def test_application_is_up(juju: jubilant.Juju):
     assert app_is_up(base_url)
 
 
-def test_relate_ingress(juju: jubilant.Juju):
-    """Relate the charm under test to the nginx ingress integrator."""
-    juju.deploy("nginx-ingress-integrator", channel="latest/stable", trust=True)
-    juju.integrate(f"{APP_NAME}:nginx-route", "nginx-ingress-integrator:nginx-route")
+def test_deploy_ingress(juju: jubilant.Juju):
+    """Deploy the ingress charm."""
+    juju.deploy("nginx-ingress-integrator", channel="latest/stable", app=INGRESS_NAME, trust=True)
+    juju.integrate(f"{APP_NAME}:nginx-route", f"{INGRESS_NAME}:nginx-route")
     juju.wait(jubilant.all_active)
+
+
+@retry(retry_num=5, retry_sleep_sec=5)
+def test_ingress_is_up(juju: jubilant.Juju):
+    """Test that the application is reachable via the ingress."""
+    status_message = juju.status().apps[INGRESS_NAME].app_status.message
+    match = re.search(r"Ingress IP\(s\): ([\d.]+)", status_message)
+    assert match, f"Could not find ingress IP in status: {status_message}"
+    ingress_ip = match.group(1)
+    session = requests.Session()
+    dns_resolver = DNSResolverHTTPAdapter(EXTERNAL_HOSTNAME, ingress_ip)
+    session.mount("http://", dns_resolver)
+    session.mount("https://", dns_resolver)
+    base_url = f"http://{EXTERNAL_HOSTNAME}"
+    assert app_is_up(base_url, session=session)
