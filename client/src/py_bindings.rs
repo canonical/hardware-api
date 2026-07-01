41edb6057c16e50a56c0016168ce88082f8eb2a6
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-07-01 13:14:21 +0200

    Return the error

--- client/src/py_bindings.rs
+++ client/src/py_bindings.rs
@@ -38,6 +38,9 @@ fn send_certification_request(py: Python, url: String) -> PyResult<Py<PyAny>> {
         native_check_certification_status(url, CheckCertificationMode::Forced, &request_body, None);
 
     if response.is_staled() {
+        let e = response
+            .stale_reason()
+            .unwrap_or("unknown reason".to_string());
         return Err(PyErr::new::<PyRuntimeError, _>(format!(
             "Request failed: {e}"
         )));
